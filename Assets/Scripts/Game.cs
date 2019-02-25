a80aefe4c394f7c34bb40536dfa6d22da394077e
Valeriy Kovalenko <vanaden1@gmail.com>
2019-02-25 20:24:40 +0300

    )))

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    public static int gridWidth = 10;
+    public static int gridHeight = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnNextTetramino();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    string GetRandomTetramino()
+    {
+        string name = "Tetramino_J";
+        switch (Random.Range(0,7))
+        {
+            case 0: name = "Tetramino_O"; break;
+            case 1: name = "Tetramino_I"; break;
+            case 2: name = "Tetramino_J"; break;
+            case 3: name = "Tetramino_L"; break;
+            case 4: name = "Tetramino_S"; break;
+            case 5: name = "Tetramino_T"; break;
+            case 6: name = "Tetramino_Z"; break;
+        }
+        return name;
+    }
+    public void SpawnNextTetramino()
+    {
+        Instantiate(Resources.Load(GetRandomTetramino()),new Vector3(5,20),Quaternion.identity);
+        
+    }
+
+    public bool CheckIsInsideGrid(Vector2 pos)
+    {
+        return ((int)pos.x >= 0 && (int)pos.x < gridWidth && (int)pos.y >= 0);
+    }
+    public Vector2 Round(Vector2 pos)
+    {
+        return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
+    }
+}
---
+++ Assets/Scripts/Tetramino.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetramino : MonoBehaviour
+{
+
+    float fall = 0;
+    public float fallSpead = 1;
+
+    public bool allowRotation = true;
+    public bool limitRotation = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //// Update is called once per frame
+    void Update()
+    {
+        CheckUserInPut();
+        Fall();
+    }
+
+    void Fall()
+    {
+        if (Time.time - fall >= fallSpead)
+        {
+            transform.position += Vector3.down;
+            if (!CheckIsValidPosition()) { transform.position += Vector3.up; }
+            fall = Time.time;
+        }
+    }
+
+    void CheckUserInPut ()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += Vector3.right;
+            if (!CheckIsValidPosition()) { transform.position += Vector3.left; }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += Vector3.left;
+            if (!CheckIsValidPosition()) { transform.position += Vector3.right; }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += Vector3.down;
+            if (!CheckIsValidPosition()) { transform.position += Vector3.up; }
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (allowRotation)
+            {
+                if (limitRotation)
+                {
+                    if (transform.rotation.eulerAngles.z >= 90)
+                    {
+                        transform.Rotate(0, 0, -90);
+                    }
+                    else
+                    {
+                        transform.Rotate(0, 0, 90);
+                    }
+                }
+                else
+                {
+                    transform.Rotate(0, 0, 90);
+                }
+                if (!CheckIsValidPosition())
+                {
+                    if (limitRotation)
+                    {
+                        if (transform.rotation.eulerAngles.z >= 90)
+                        {
+                            transform.Rotate(0, 0, -90);
+                        }
+                        else
+                        {
+                            transform.Rotate(0, 0, 90);
+                        }
+                    }
+                    else
+                    {
+                        transform.Rotate(0, 0, -90);
+                    }
+                }
+
+            }
+            
+        }
+    }
+
+    bool CheckIsValidPosition()
+    {
+        foreach(Transform mino in transform)
+        {
+            Vector2 pos = FindObjectOfType<Game>().Round(mino.position);
+            if (FindObjectOfType<Game>().CheckIsInsideGrid(pos) == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
